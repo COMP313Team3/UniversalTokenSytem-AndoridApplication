1305a14563234e21179892de1d5d853c451e4586
evlynmaria <evlyn.maria.peter@gmail.com>
2015-11-08 23:45:41 -0500

    Converting the Campus Data into List

--- UniversalTokenSystem/src/com/example/universaltokensystem/DepartmentInfo.java
+++ UniversalTokenSystem/src/com/example/universaltokensystem/DepartmentInfo.java
@@ -1,43 +1,22 @@
 package com.example.universaltokensystem;
 
-import java.io.IOException;
-import java.io.InputStream;
 import java.util.ArrayList;
-
-import org.apache.http.HttpEntity;
-import org.apache.http.HttpResponse;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.impl.client.DefaultHttpClient;
-import org.apache.http.protocol.BasicHttpContext;
-import org.apache.http.protocol.HttpContext;
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
 
 import android.app.Activity;
 import android.content.Intent;
-import android.os.AsyncTask;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.view.View;
-import android.widget.AdapterView;
-import android.widget.AdapterView.OnItemClickListener;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
 import android.widget.TextView;
-import android.widget.Toast;
+
 public class DepartmentInfo extends Activity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.department_info);  
-        TextView campusInfo = (TextView)findViewById(R.id.CampusInfo);
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.department_info);
+		TextView campusInfo = (TextView) findViewById(R.id.CampusInfo);
 		Intent intent = getIntent();
-		String campusList = "Welcome to "+ getIntent().getExtras().getString("CampusInfo").toString()+" Campus";
-		campusInfo.setText(campusList);
-    }  
+		String campusList = "Welcome to " + intent.getExtras().getString("CampusInfo").toString() + " Campus";
+		ArrayList<String> campusData = intent.getExtras().getStringArrayList("CampusData");
+		campusInfo.setText("Campus ID:"+campusData.get(0)+" , Campus Name:"+campusData.get(1)+" , Campus Address:"+campusData.get(2));
+	}
 }
-
