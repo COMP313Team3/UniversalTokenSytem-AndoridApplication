e4441c6e1b49bc83116f5dae66d1a80189db2e90
srichatala <sri2vas@gmail.com>
2015-11-17 17:37:36 -0500

    Task #119 to avoid merge conflicts

--- CCTokenSystem/CCTokenSystem/obj/Release/Package/PackageTmp/Scripts/_references.js
+++ CCTokenSystem/CCTokenSystem/obj/Release/Package/PackageTmp/Scripts/_references.js
@@ -6,10 +6,7 @@
 /// <reference path="controller/campusctrl/campusctrl.js" />
 /// <reference path="controller/deptctrl/deptctrl.js" />
 /// <reference path="controller/studentctrl/studentctrl.js" />
-<<<<<<< HEAD
-=======
 /// <reference path="controller/tokenctrl/tokenctrl.js" />
->>>>>>> 5fc3a79c57cc38a70dfd85fc8cc39e7bc0fd4d26
 /// <reference path="jquery-2.1.4.js" />
 /// <reference path="jquery-2.1.4.min.js" />
 /// <reference path="modernizr-2.6.2.js" />
